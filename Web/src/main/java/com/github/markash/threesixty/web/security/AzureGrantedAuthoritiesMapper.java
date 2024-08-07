f10540d48142bb21d7493510a2d944a2180feaf7
mpashworth <mp.ashworth@gmail.com>
2024-08-07 21:56:13 +0200

    feat(security): Map application roles in the OIDC_USER authority to Simple Granted Authority instances.

--- Web/src/main/java/com/github/markash/threesixty/web/security/AzureGrantedAuthoritiesMapper.java
+++ Web/src/main/java/com/github/markash/threesixty/web/security/AzureGrantedAuthoritiesMapper.java
@@ -3,9 +3,12 @@ package com.github.markash.threesixty.web.security;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -16,8 +19,24 @@ public class AzureGrantedAuthoritiesMapper implements GrantedAuthoritiesMapper {
     @Override
     public Collection<? extends GrantedAuthority> mapAuthorities(Collection<? extends GrantedAuthority> authorities) {
 
-        authorities.forEach(a -> LOG.info("Granted authority: {}", a));
+        List<SimpleGrantedAuthority> grantedAuthorities = authorities
+            .stream()
+            .filter(authority -> authority.getAuthority().equalsIgnoreCase("OIDC_USER"))
+            .map(authority -> {
+                if (authority instanceof OidcUserAuthority userAuthority && userAuthority.getAttributes().containsKey("roles")) {
+                    if (userAuthority.getAttributes().get("roles") instanceof List<?> roles) {
+                        return roles.stream()
+                                    .map(role -> new SimpleGrantedAuthority(role.toString()))
+                                    .toList();
+                    }
+                }
+                return new ArrayList<SimpleGrantedAuthority>();
+            })
+            .flatMap(Collection::stream)
+            .toList();
 
-        return authorities;
+        grantedAuthorities.forEach(x -> LOG.info("GrantedAuthority: {}", x));
+
+        return grantedAuthorities;
     }
 }
