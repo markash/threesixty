c1155fee61f6cf283aa729357e35019daa267ed6
mpashworth <mp.ashworth@gmail.com>
2024-08-12 22:08:36 +0200

    chore: Configure multiple deployments and code quality check

---
+++ Assessment/src/main/java/com/github/markash/threesixty/assessment/AssessmentApplication.java
@@ -0,0 +1,12 @@
+package com.github.markash.threesixty.assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentApplication
+{
+    public static void main( String[] args ) {
+        SpringApplication.run(AssessmentApplication.class, args);
+    }
+}
